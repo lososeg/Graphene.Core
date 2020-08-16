f1d79f4edc5099213a011c7fafecb13298b59483
Sergey S <ya.lososeg@gmail.com>
2020-08-16 05:30:56 +0300

    Added the ability to connect to http nodes

---
+++ HttpManager.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Graphene.Core.Interfaces;
+using Graphene.Core.JsonRpc;
+using Newtonsoft.Json;
+
+namespace Graphene.Core
+{
+    public class HttpManager : IConnectionManager
+    {
+        public string UrlToConnect { get; set; }
+
+        public bool IsConnected => !string.IsNullOrEmpty(UrlToConnect);
+
+        public IHttpClient HttpClient { get; set; }
+
+
+        public HttpManager(IHttpClient httpClient)
+        {
+            HttpClient = httpClient;
+        }
+
+        /// <summary>
+        /// Connects and checks http status
+        /// </summary>  
+        /// <param name="requestUrl">The Uri the request is sent to.</param>
+        /// <param name="token">Throws a <see cref="T:System.OperationCanceledException" /> if this token has had cancellation requested.</param>
+        /// <returns>Url which will be used for data transfer (empty if none)</returns>
+        /// <exception cref="T:System.OperationCanceledException">The token has had cancellation requested.</exception>
+        /// <exception cref="T:System.ArgumentNullException">The requestUri was null.</exception>
+        /// <exception cref="T:System.Net.Http.HttpRequestException">The request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.</exception>
+        public async Task<bool> ConnectToAsync(string requestUrl, CancellationToken token)
+        {
+            UrlToConnect = string.Empty;
+
+            if (string.IsNullOrEmpty(requestUrl))
+                return false;
+
+            var response = await HttpClient.GetAsync(requestUrl, token).ConfigureAwait(false);
+            if (response.IsSuccessStatusCode)
+            {
+                UrlToConnect = requestUrl;
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Clears connection url
+        /// </summary>
+        public void Disconnect()
+        {
+            UrlToConnect = string.Empty;
+        }
+
+        /// <summary>
+        /// Sends request to specified url
+        /// </summary>
+        /// <typeparam name="T">Some type for response deserialization</typeparam>
+        /// <param name="jsonRpc">Request body</param>
+        /// <param name="jsonSerializerSettings">Specifies json settings</param>
+        /// <param name="token">Throws a <see cref="T:System.OperationCanceledException" /> if this token has had cancellation requested.</param>
+        /// <returns>Typed JsonRpcResponse</returns>
+        public async Task<JsonRpcResponse<T>> ExecuteAsync<T>(IJsonRpcRequest jsonRpc, JsonSerializerSettings jsonSerializerSettings, CancellationToken token)
+        {
+            return await RepeatExecuteAsync<T>(jsonRpc, jsonSerializerSettings, 0, token).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Sends request to specified url.
+        /// </summary>
+        /// <typeparam name="T">Some type for response deserialization</typeparam>
+        /// <param name="jsonRpc">Request body</param>
+        /// <param name="jsonSerializerSettings">Specifies json settings</param>
+        /// <param name="token">Throws a <see cref="T:System.OperationCanceledException" /> if this token has had cancellation requested.</param>
+        /// <returns>Typed JsonRpcResponse</returns>
+        public async Task<JsonRpcResponse<T>> RepeatExecuteAsync<T>(IJsonRpcRequest jsonRpc, JsonSerializerSettings jsonSerializerSettings, CancellationToken token)
+        {
+            return await RepeatExecuteAsync<T>(jsonRpc, jsonSerializerSettings, HttpClient.MaxRequestRepeatCount, token).ConfigureAwait(false);
+        }
+
+        private async Task<JsonRpcResponse<T>> RepeatExecuteAsync<T>(IJsonRpcRequest jsonRpc, JsonSerializerSettings jsonSerializerSettings, byte loop, CancellationToken token)
+        {
+            if (string.IsNullOrEmpty(UrlToConnect))
+                return new JsonRpcResponse<T>(new ArgumentNullException(nameof(UrlToConnect))) { RawRequest = jsonRpc.Message };
+
+            try
+            {
+                var content = new StringContent(jsonRpc.Message, Encoding.UTF8, "application/json");
+                var response = await HttpClient.PostAsync(UrlToConnect, content, loop, token).ConfigureAwait(false);
+
+                var stringResponse = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                var prop = JsonConvert.DeserializeObject<JsonRpcResponse<T>>(stringResponse, jsonSerializerSettings);
+                prop.RawRequest = jsonRpc.Message;
+                prop.RawResponse = stringResponse;
+                return prop;
+            }
+            catch (Exception ex)
+            {
+                return new JsonRpcResponse<T>(ex)
+                {
+                    RawRequest = jsonRpc.Message
+                };
+            }
+        }
+    }
+}
---
+++ RepeatHttpClient.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Graphene.Core.Interfaces;
+
+namespace Graphene.Core
+{
+    public class RepeatHttpClient : HttpClient, IHttpClient
+    {
+        private static readonly Random Random = new Random();
+        private readonly Dictionary<HttpClient, int> _httpClientSet;
+        private HttpClient _current;
+
+        public byte MaxRequestRepeatCount { get; }
+
+
+        public RepeatHttpClient()
+            : this(3, 1024 * 1024) { }
+
+        public RepeatHttpClient(byte maxRequestRepeatCount, long maxResponseContentBufferSize)
+        {
+            MaxRequestRepeatCount = maxRequestRepeatCount;
+            var client = new HttpClient
+            {
+                MaxResponseContentBufferSize = maxResponseContentBufferSize
+            };
+            _httpClientSet = new Dictionary<HttpClient, int> { { client, 0 } };
+            _current = client;
+        }
+
+
+        public async Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content, byte loopCount, CancellationToken token)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(requestUri),
+                Content = content,
+            };
+            return await SendAsync(request, loopCount, token).ConfigureAwait(false);
+        }
+
+        public override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken token)
+        {
+            var loopCount = request.Method == HttpMethod.Post ? (byte)0 : MaxRequestRepeatCount;
+            return SendAsync(request, loopCount, token);
+        }
+
+        public virtual async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, byte loopCount, CancellationToken token)
+        {
+            var isfirst = true;
+            var maxL = 0;
+            do
+            {
+                //prevent "System.InvalidOperationException: Cannot send the same request message multiple times"
+                var cloneRequest = await CloneHttpRequestMessageAsync(request).ConfigureAwait(false);
+
+                maxL++;
+                HttpClient client = null;
+                try
+                {
+                    if (isfirst)
+                        isfirst = false;
+                    else
+                        await Task.Delay(1000 * maxL + Random.Next(1, 5) * 100, token).ConfigureAwait(false);
+
+                    lock (_httpClientSet)
+                    {
+                        client = _current;
+                        _httpClientSet[client]++;
+                    }
+
+                    var msg = await client.SendAsync(cloneRequest, token).ConfigureAwait(false);
+                    if (msg.IsSuccessStatusCode || maxL >= loopCount)
+                        return msg;
+
+                    ResetClient(client);
+                }
+                catch (Exception)
+                {
+                    ResetClient(client);
+
+                    if (token.IsCancellationRequested)
+                        throw;
+
+                    if (maxL >= loopCount)
+                        throw;
+                }
+                finally
+                {
+                    if (client != null)
+                    {
+                        lock (_httpClientSet)
+                        {
+                            _httpClientSet[client]--;
+                            if (client != _current && _httpClientSet[client] < 1)
+                            {
+                                _httpClientSet.Remove(client);
+                                client.Dispose();
+                            }
+                        }
+                    }
+                }
+            } while (true);
+        }
+
+        public static async Task<HttpRequestMessage> CloneHttpRequestMessageAsync(HttpRequestMessage req)
+        {
+            var clone = new HttpRequestMessage(req.Method, req.RequestUri)
+            {
+                Version = req.Version
+            };
+
+            // Copy the request's content (via a MemoryStream) into the cloned object
+            var ms = new MemoryStream();
+            if (req.Content != null)
+            {
+                await req.Content.CopyToAsync(ms).ConfigureAwait(false);
+                ms.Position = 0;
+                clone.Content = new StreamContent(ms);
+
+                // Copy the content headers
+                if (req.Content.Headers != null)
+                    foreach (var h in req.Content.Headers)
+                        clone.Content.Headers.Add(h.Key, h.Value);
+            }
+
+            foreach (var prop in req.Properties)
+                clone.Properties.Add(prop);
+
+            foreach (var header in req.Headers)
+                clone.Headers.TryAddWithoutValidation(header.Key, header.Value);
+
+            return clone;
+        }
+
+
+        private void ResetClient(HttpClient oldClient)
+        {
+            lock (_httpClientSet)
+            {
+                if (oldClient == _current)
+                {
+                    _current = new HttpClient
+                    {
+                        MaxResponseContentBufferSize = MaxResponseContentBufferSize
+                    };
+                    _httpClientSet.Add(_current, 0);
+                }
+            }
+        }
+    }
+}
